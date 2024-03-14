750216e579ea61514dbe46aba46f32fb3131b0e1
Ben Tomasini <Ben.Tomasini@authwise.com>
2024-03-14 11:26:14 -0700

    updated modules structure

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * as database from './database';
+export * as gke from './gke';
+export * as storage from './storage';
+export * as twingate from './twingate';
+export * as verifiedcertificate from './verifiedcertificate';
